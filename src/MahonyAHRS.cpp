9d2cf96604d42a78b93da7ece3b1546e169866fd
PaulStoffregen <paul@pjrc.com>
2022-09-17 04:43:09 -0700

    Fix compiler warning

--- src/MahonyAHRS.cpp
+++ src/MahonyAHRS.cpp
@@ -249,10 +249,10 @@ void Mahony::updateIMU(float gx, float gy, float gz, float ax, float ay, float a
 float Mahony::invSqrt(float x)
 {
 	float halfx = 0.5f * x;
-	float y = x;
-	long i = *(long*)&y;
-	i = 0x5f3759df - (i>>1);
-	y = *(float*)&i;
+	union { float f; long l; } i;
+	i.f = x;
+	i.l = 0x5f3759df - (i.l >> 1);
+	float y = i.f;
 	y = y * (1.5f - (halfx * y * y));
 	y = y * (1.5f - (halfx * y * y));
 	return y;
